be7d3792b756ce2113ab366ef9bf5013e250ce32
Jamie Nelson <jamie@theinstillery.com>
2018-06-20 16:27:52 +0100

    Initial commit of policy generator module

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-variable "parameter_root_name" {
-  description = "The prefix or root parameter that you want to allow access to"
-  default     = ""
-}
-
-variable "kms_key" {
-  description = "The arn of the KMS key that you want to allow access to. If empty it uses a wildcard resource. `*` "
-  default     = ""
-}
-
-variable "region" {
-  description = "The region of the parameter store value that you want to allow access to. If none supplied, it uses the current region of the provider."
-  default     = ""
-}
-
-variable "account_id" {
-  description = "The account id of the parameter store you want to allow access to. If none supplied, it uses the current account id of the provider. "
-  default     = ""
-}
-
 data "aws_region" "default" {}
 data "aws_caller_identity" "default" {}
 
@@ -83,24 +63,4 @@ data "aws_iam_policy_document" "manage_kms_store" {
       "${local.kms_key}",
     ]
   }
-}
-
-output "read_parameter_store_policy" {
-  value = "${data.aws_iam_policy_document.read_parameter_store.json}"
-}
-
-output "write_parameter_store_policy" {
-  value = "${data.aws_iam_policy_document.write_parameter_store.json}"
-}
-
-output "manage_kms_store_policy" {
-  value = "${data.aws_iam_policy_document.manage_kms_store.json}"
-}
-
-output "manage_parameter_store_policy" {
-  value = "${data.aws_iam_policy_document.manage_parameter_store.json}"
-}
-
-output "put_xray_trace_policy" {
-  value = "${data.aws_iam_policy_document.put_xray_trace.json}"
 }
