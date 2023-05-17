0ba3f13f5d9b2fd62fb8b381859c48aa7a8876fe
Max Lobur <max_lobur@outlook.com>
2023-05-17 16:48:00 +0300

    Sync github (#9)

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ data "aws_region" "default" {}
 data "aws_caller_identity" "default" {}
 
 locals {
-  region     = "${ var.region == "" ? data.aws_region.default.name : var.region}"
-  account_id = "${var.account_id == "" ? data.aws_caller_identity.default.account_id : var.account_id}"
+  region     = var.region == "" ? data.aws_region.default.name : var.region
+  account_id = var.account_id == "" ? data.aws_caller_identity.default.account_id : var.account_id
 
   # Normalise the parameter name, and remove any duplicate slashes
-  parameter_root_name = "${join("/",compact(split("/", var.parameter_root_name)))}"
+  parameter_root_name = join("/", compact(split("/", var.parameter_root_name)))
 
   # If no KMS arn supplied, allow access to any KMS 
-  kms_key = "${var.kms_key == "" ? "*" : var.kms_key }"
+  kms_key = var.kms_key == "" ? "*" : var.kms_key
 }
 
 data "aws_iam_policy_document" "read_parameter_store" {
